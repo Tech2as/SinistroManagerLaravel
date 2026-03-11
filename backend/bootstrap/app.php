46cf8d2fe2c20790d249af00de33f229a3a78fe9
Tech2as <techeletrum@gmail.com>
2026-03-11 14:43:02 -0300

    refactor: retirando a migration duplicada do users

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api/admin',
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
